a6a6e4c3ad5158ad6f15723312b3bcbab26bbd92
gencdumi <gnc.dum@gmail.com>
2025-12-03 19:46:01 +0200

    feat: add modapp

---
+++ src/gr/aueb/cf/testgit/ModApp.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.testgit;
+
+public class ModApp {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int mof(int a, int b) {
+        return a % b;
+
+    }
+}
